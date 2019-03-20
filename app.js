f77ffe9d92160376288930ffb3d2414d0d98a26e
MeeraRemesh <meeraremesh007@gmail.com>
2019-03-20 15:20:20 +0530

    module

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const express=require('express');
+const hbs=require('hbs');
 var app=express();
+app.set('view engine','hbs');
 app.get('/',(req,res)=>{
-res.send('welcome to my website');
+res.render('index.hbs');
 });
 app.get('/home',(req,res)=>{
 res.send('Welcome To My Home Page');
 });
 app.get('/contact',(req,res)=>{
-    res.send('Welcome To My Contact Page');
+    res.send('<h1>Welcome To My Contact Page</h1>');
     })
 app.listen(3000);
